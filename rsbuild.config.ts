fb8b498d6a5c06c6591e67132e819ef1d5e6275e
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2024-08-15 15:10:36 +0200

    Add env var issue

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,14 @@
+import { loadEnv } from "@rsbuild/core";
 import { defineConfig } from "@rsbuild/core";
 
+const { publicVars } = loadEnv({
+  prefixes: ["REACT_APP_"],
+});
+
 export default defineConfig({
+  source: {
+    define: publicVars,
+  },
   output: {
     distPath: {
       root: "./rsbuild-dist",
--- src/render.js
+++ src/render.js
@@ -1,8 +1,16 @@
-import './render.css'
+import "./render.css";
 
 export function render() {
-    const el = document.createElement('div')
-    el.classList.add('text')
-    document.getElementsByTagName('body')[0].appendChild(el)
-    el.innerHTML = 'hello, world'
+  const el = document.createElement("div");
+
+  // This will not work, even though loadEnv is used in rsbuild.config.js
+  const envMsg = process.env.REACT_APP_TEST_ENV;
+
+  // Would work:
+  // const envMsg = process.env.NODE_ENV;
+
+  el.classList.add("text");
+  document.getElementsByTagName("body")[0].appendChild(el);
+
+  el.innerHTML = "hello, world " + envMsg;
 }
